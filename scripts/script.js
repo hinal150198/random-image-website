eabb2c62c0127058c6070e47783e73f59128a1b9
hinal150198 <36818885+hinal150198@users.noreply.github.com>
2024-10-12 11:57:13 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,8 @@
 function showRandomImage() {
   const images = [
-    "https://drive.google.com/drive/u/1/folders/1Jxl-AM2FHGyWRAUefkO1YDWK1SB8sngM",
-    "https://drive.google.com/drive/u/1/folders/1Jxl-AM2FHGyWRAUefkO1YDWK1SB8sngM",
-    "https://drive.google.com/drive/u/1/folders/1Jxl-AM2FHGyWRAUefkO1YDWK1SB8sngM",
-    "https://drive.google.com/drive/u/1/folders/1Jxl-AM2FHGyWRAUefkO1YDWK1SB8sngM",
-    "https://drive.google.com/drive/u/1/folders/1Jxl-AM2FHGyWRAUefkO1YDWK1SB8sngM",  
+    "https://drive.google.com/uc?id=1Jxl-AM2FHGyWRAUefkO1YDWK1SB8sngM",
+    "https://drive.google.com/uc?id=1Jxl-AM2FHGyWRAUefkO1YDWK1SB8sngM",
+     
     // Add more image links from Google Drive here
   ];
 
